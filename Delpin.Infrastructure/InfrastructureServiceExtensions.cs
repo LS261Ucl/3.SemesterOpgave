04474e0b7c8efd4b520ccd0754810adc408f3ee8
Zorxh <Zorxh96@gmail.com>
2021-05-08 09:56:19 +0200

    Added Identity context and user to project.

--- Delpin.Infrastructure/InfrastructureServiceExtensions.cs
+++ Delpin.Infrastructure/InfrastructureServiceExtensions.cs
@@ -1,6 +1,7 @@
 ﻿using Delpin.Application.Interfaces;
 using Delpin.Infrastructure.Data;
 using Delpin.Infrastructure.Data.Repositories;
+using Delpin.Infrastructure.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -15,6 +16,11 @@ namespace Delpin.Infrastructure
             services.AddDbContext<DelpinContext>(opt =>
             {
                 opt.UseSqlServer(configuration.GetConnectionString("DefaultConnection"));
+            });
+
+            services.AddDbContext<DelpinIdentityContext>(opt =>
+            {
+                opt.UseSqlServer(configuration.GetConnectionString("IdentityConnection"));
             });
 
             services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
