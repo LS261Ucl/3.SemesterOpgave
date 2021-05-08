1dc002962cf342ce3db96350f58777131afa5a3e
Zorxh <Zorxh96@gmail.com>
2021-05-08 10:45:12 +0200

    Added Dtos to handle login/register

---
+++ Delpin.Application/Contracts/v1/Identity/RegisterDto.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Delpin.Application.Contracts.v1.Identity
+{
+    public class RegisterDto
+    {
+        [Required]
+        public string FullName { get; set; }
+        
+        [Required, EmailAddress]
+        public string Email { get; set; }
+        
+        [Required]
+        public string Password { get; set; }
+    }
+}
