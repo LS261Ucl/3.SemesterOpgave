b39f04d20a448a6d7ef6a4fefa7f46ec30b12fd8
Mikkel Buhl <zorxh96@gmail.com>
2021-05-19 13:30:32 +0200

    Added RentalsController and create booking functionality

---
+++ Delpin.MVC/Controllers/RentalsController.cs
@@ -0,0 +1,93 @@
+﻿using Delpin.Mvc.Extensions;
+using Delpin.Mvc.Models.Rentals;
+using Delpin.Mvc.Services;
+using Delpin.MVC.Dto.v1.ProductItems;
+using Delpin.MVC.Dto.v1.Products;
+using Delpin.MVC.Dto.v1.RentalLines;
+using Delpin.MVC.Dto.v1.Rentals;
+using Delpin.MVC.Services;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Delpin.Mvc.Controllers
+{
+    public class RentalsController : Controller
+    {
+        private readonly IShoppingCartService _shoppingCartService;
+        private readonly IHttpService _httpService;
+
+        public RentalsController(IShoppingCartService shoppingCartService, IHttpService httpService)
+        {
+            _shoppingCartService = shoppingCartService;
+            _httpService = httpService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> ShoppingCart(string email)
+        {
+            var shoppingCart = _shoppingCartService.GetShoppingCart(email);
+
+            if (shoppingCart == null || shoppingCart.Count <= 0)
+                return View(new CreateRentalViewModel());
+
+            var rental = new CreateRentalViewModel();
+
+            foreach (var item in shoppingCart)
+            {
+                rental.RentalLines.Add(new CreateRentalLineDto { ProductItemId = item.Id });
+
+                var productResponse = await _httpService.Get<ProductDto>($"Products/{item.ProductId}", User.GetToken());
+
+                rental.Products.Add(productResponse.Response);
+            }
+
+            return View(rental);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> ShoppingCart(CreateRentalViewModel rentalViewModel)
+        {
+            CreateRentalDto rentalDto = new CreateRentalDto
+            {
+                CustomerId = rentalViewModel.CustomerId,
+                StartDate = rentalViewModel.StartDate,
+                EndDate = rentalViewModel.EndDate,
+                Address = rentalViewModel.Address,
+                PostalCode = rentalViewModel.PostalCode,
+                RentalLines = new List<CreateRentalLineDto>()
+            };
+
+            foreach (var item in _shoppingCartService.GetShoppingCart(User.GetEmail()))
+            {
+                rentalDto.RentalLines.Add(new CreateRentalLineDto { ProductItemId = item.Id });
+
+                var updateProductItem = new UpdateProductItemDto
+                {
+                    IsAvailable = false,
+                    PostalCode = rentalViewModel.PostalCode,
+                    LastService = item.LastService
+                };
+
+                await _httpService.Update($"ProductItems/{item.Id}", updateProductItem, User.GetToken());
+            }
+
+            try
+            {
+                var response = await _httpService.Create<CreateRentalDto, RentalDto>("Rentals", rentalDto, User.GetToken());
+
+                if (!response.Success)
+                {
+                    return View(rentalViewModel);
+                }
+
+                return RedirectToAction("Index", "Home");
+            }
+            catch
+            {
+                return View(rentalViewModel);
+            }
+        }
+    }
+}
---
+++ Delpin.MVC/Models/Rentals/CreateRentalViewModel.cs
@@ -0,0 +1,38 @@
+﻿using Delpin.MVC.Dto.v1.Products;
+using Delpin.MVC.Dto.v1.RentalLines;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Delpin.Mvc.Models.Rentals
+{
+    public class CreateRentalViewModel
+    {
+        [Required]
+        [Display(Name = "Kundenummer")]
+        public string CustomerId { get; set; }
+
+        [Required]
+        [DataType(DataType.Date)]
+        [Display(Name = "Start dato")]
+        public DateTime StartDate { get; set; } = DateTime.UtcNow;
+
+        [Required]
+        [DataType(DataType.Date)]
+        [Display(Name = "Slut dato")]
+        public DateTime EndDate { get; set; } = DateTime.UtcNow.AddDays(5);
+
+        [Required]
+        [Display(Name = "Adresse")]
+        public string Address { get; set; }
+
+        [Required]
+        [Range(0, 9999)]
+        [Display(Name = "Postnummer")]
+        public string PostalCode { get; set; }
+
+        [Required] public List<CreateRentalLineDto> RentalLines { get; set; } = new List<CreateRentalLineDto>();
+
+        public List<ProductDto> Products { get; set; } = new List<ProductDto>();
+    }
+}
